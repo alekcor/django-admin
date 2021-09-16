3c44eabf111c8caacc649c65a50d565c6a273d8c
alekcor <89666868+alekcor@users.noreply.github.com>
2021-09-16 07:19:53 +0200

    clear filters

--- client/src/components/table/table.jsx
+++ client/src/components/table/table.jsx
@@ -1,7 +1,7 @@
 import 'antd/dist/antd.css';
 import {BuildColumns} from "./columns";
 import {Component} from 'react';
-import {Button, Dropdown, message, Space, Table} from 'antd';
+import {Button, Dropdown, Space, Table} from 'antd';
 import {DownOutlined} from '@ant-design/icons';
 import {Overlay} from "../dropdown-overlay/Overlay";
 
@@ -10,12 +10,17 @@ class ModelTable extends Component {
   state = {
     filteredInfo: null,
     sortedInfo: null,
-    data: null
+    data: null,
+    selectedRowKeys: []
   };
 
   columns() {
     const { sortedInfo, filteredInfo } = this.state;
     return BuildColumns(sortedInfo, filteredInfo)
+  }
+
+  onRowSelectionChange = (selectedRowKeys) => {
+    this.setState({selectedRowKeys})
   }
 
   handleChange = (pagination, filters, sorter) => {
@@ -25,11 +30,7 @@ class ModelTable extends Component {
     });
   };
 
-  clearFilters = () => {
-    this.setState({ filteredInfo: null });
-  };
-
-  clearAll = () => {
+  clearFiltersAndSorters = () => {
     this.setState({
       filteredInfo: null,
       sortedInfo: null,
@@ -45,8 +46,11 @@ class ModelTable extends Component {
     });
   };
 
-  onMenuItemClick({key}) {
-    message.info(`User ${key} was deleted`)
+  onMenuItemClick= () => {
+    const {selectedRowKeys, data} = this.state
+
+    const updatedDate = data.filter(item => !selectedRowKeys.includes(item.key))
+    this.setState({data: updatedDate})
   }
 
   componentDidMount() {
@@ -56,8 +60,11 @@ class ModelTable extends Component {
   }
 
   render() {
-    const { data } = this.state;
-
+    const { data, selectedRowKeys } = this.state;
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onRowSelectionChange
+    }
     return (
       <>
         <Space style={{ marginBottom: 16 }}>
@@ -65,10 +72,13 @@ class ModelTable extends Component {
             <Button>Actions<DownOutlined/></Button>
           </Dropdown>
           <Button onClick={this.setAgeSort}>Sort age</Button>
-          <Button onClick={this.clearFilters}>Clear filters</Button>
-          <Button onClick={this.clearAll}>Clear filters and sorters</Button>
+          <Button type={"link"} onClick={this.clearFiltersAndSorters}>Clear</Button>
         </Space>
-        <Table rowSelection={{type: "checkbox"}} columns={this.columns()} dataSource={data} onChange={this.handleChange} />
+        <Table
+          rowSelection={rowSelection}
+          columns={this.columns()}
+          dataSource={data}
+          onChange={this.handleChange} />
       </>
     );
   }
